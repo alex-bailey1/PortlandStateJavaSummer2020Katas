b49dbbf54cc3d042b086777773587b8d40d0814a
David Whitlock <david.m.whitlock@gmail.com>
2020-07-08 19:41:30 -0700

    Added the first unit test.

--- RomanNumerals/src/test/java/edu/pdx/cs410J/bail34/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/bail34/KataTest.java
@@ -2,12 +2,15 @@ package edu.pdx.cs410J.bail34;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
   @Test
-  public void canInstantiateKataClass() {
-    new Kata();
+  public void romanNumeralFor1IsI() {
+    assertThat(Kata.romanNumeralFor(1), equalTo("I"));
   }
 
 }
